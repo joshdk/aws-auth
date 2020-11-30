d43de7fa6f41a5030eeb17873524c6eb79022b31
Josh Komoroske <jdkomo@gmail.com>
2020-11-29 20:47:23 -0800

    Create credential transform chains from AWS profile

--- config/config.go
+++ config/config.go
@@ -19,6 +19,124 @@ type Config struct {
 	credentials *ini.File
 }
 
+type User struct {
+	AWSAccessKeyID     string
+	AWSSecretAccessKey string
+	AWSSessionToken    string
+}
+
+type Role struct {
+	DurationSeconds int
+	ExternalID      string
+	MFASerial       string
+	RoleARN         string
+	RoleSessionName string
+	SourceProfile   string
+}
+
+// sectionAsUser takes the given ini.Section and converts it to a User if all
+// of the required fields are present.
+func sectionAsUser(section *ini.Section) *User {
+	// Pack section values into struct.
+	// https://docs.aws.amazon.com/cli/latest/topic/config-vars.html#credentials
+	user := User{
+		AWSAccessKeyID:     section.Key("aws_access_key_id").Value(),
+		AWSSecretAccessKey: section.Key("aws_secret_access_key").Value(),
+		AWSSessionToken:    section.Key("aws_session_token").Value(),
+	}
+
+	// Verify that required fields are present.
+	switch {
+	case user.AWSAccessKeyID == "":
+		return nil
+	case user.AWSSecretAccessKey == "":
+		return nil
+	default:
+		return &user
+	}
+}
+
+// sectionAsRole takes the given ini.Section and converts it to a Role if all
+// of the required fields are present.
+func sectionAsRole(section *ini.Section) *Role {
+	// Pack section values into struct.
+	// https://docs.aws.amazon.com/cli/latest/topic/config-vars.html#using-aws-iam-roles
+	role := Role{
+		ExternalID:      section.Key("external_id").Value(),
+		MFASerial:       section.Key("mfa_serial").Value(),
+		RoleARN:         section.Key("role_arn").Value(),
+		RoleSessionName: section.Key("role_session_name").Value(),
+		SourceProfile:   section.Key("source_profile").Value(),
+	}
+
+	// Use the given duration, or fall back to a 1 hour default.
+	if duration, err := section.Key("duration_seconds").Int(); err == nil {
+		role.DurationSeconds = duration
+	} else {
+		role.DurationSeconds = 3600 // 1 hour
+	}
+
+	// Verify that required fields are present.
+	switch {
+	case role.RoleARN == "":
+		return nil
+	case role.SourceProfile == "":
+		return nil
+	default:
+		return &role
+	}
+}
+
+// Profile finds the named profile, and returns only one of either a User
+// (which contains credentials), or a Role (which describes how to derive
+// credentials). In the event that the named profile does not exist (or is
+// otherwise misconfigured), both return values will be nil.
+func (c *Config) Profile(name string) (*User, *Role) {
+	section, found := c.profile(name)
+
+	// Section is missing altogether.
+	if !found {
+		return nil, nil
+	}
+
+	// Section contains valid User settings.
+	if user := sectionAsUser(section); user != nil {
+		return user, nil
+	}
+
+	// Section contains valid Role settings.
+	if role := sectionAsRole(section); role != nil {
+		return nil, role
+	}
+
+	// Section doesn't contain any valid settings.
+	return nil, nil
+}
+
+// profile looks up the given section name from the AWS config/credentials
+// file, following the rules for section naming and precedence in those files.
+func (c *Config) profile(name string) (*ini.Section, bool) {
+	// Look up the profile name directly in the credentials file.
+	if section, err := c.credentials.GetSection(name); err == nil {
+		return section, true
+	}
+
+	// In the config file, profile names are prefixed with "profile ", except
+	// for the "default" profile which is used verbatim.
+	// "default" → "default"
+	// "example" → "profile example"
+	if name != "default" {
+		name = "profile " + name
+	}
+
+	// Look up the (possibly prefixed) profile name in the config file.
+	if section, err := c.config.GetSection(name); err == nil {
+		return section, true
+	}
+
+	return nil, false
+}
+
 func Load() (*Config, error) {
 	// Determine the location of the AWS config file.
 	configFile := filepath.Join(userHomeDir(), ".aws", "config")
